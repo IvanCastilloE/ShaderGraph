b89f54203a0fe86fee92348cdf46f6df7cd1b435
Angel Ivan Castillo <angel_xayan06@hotmail.com>
2019-05-07 19:51:44 -0700

    Usamos la herramienta ShaderGraph para hacer  Shaders

---
+++ ShaderGraph_TopicosFisica/Assets/Shaders/CustomLightNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.ShaderGraph;
+using System.Reflection;
+
+
+public class CustomLightNode:CodeFunctionNode
+{
+    public override bool hasPreview { get => false;  }
+
+
+    protected override MethodInfo GetFunctionToConvert()
+    {
+
+    }
+}
